8c8cf8e1ad3f121c587ef6038091668cb8273acf
Ajarek <ajarek@poczta.onet.pl>
2023-06-19 22:55:58 +0200

    added FormHome

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { FormHome } from '../../helper/FormHome/FormHome'
 import './Home.css'
 
 const Home = () => {
+  const [dataFormHome, setDataFormHome] = useState(null)
+  const [alert, setAlert] = useState(false)
   const navigate = useNavigate()
-  
+  const handleFormHome = (data) => {
+   data.from === data.to?setAlert(true):(setDataFormHome(data),setAlert(false))
+    
+  }
+  console.log(dataFormHome)
   return (
     <div className='home'>
-      <h1>Book a Bus</h1>
-       
+      <section className='section'>
+        <h1>Book a Bus</h1>{alert?<div style={{color:'red'}}>cities no different</div>:null}
+        <FormHome onSubmit={handleFormHome} />
+      </section>
     </div>
   )
 }
