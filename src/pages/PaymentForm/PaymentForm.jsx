ee8e317f987559325980e5b76bbd91b8abb5521a
Ajarek <ajarek@poczta.onet.pl>
2023-06-24 23:45:21 +0200

    added Modal

--- src/pages/PaymentForm/PaymentForm.jsx
+++ src/pages/PaymentForm/PaymentForm.jsx
@@ -1,15 +1,27 @@
 import { React, useState, useContext, useEffect, useRef } from 'react'
 import { AppContext } from '../../App'
+import { redirect, useParams, useNavigate,Link } from 'react-router-dom'
 import './PaymentForm.css'
 import {FormPayment} from '../../helper/FormPayment/FormPayment'
+import Modal from '../../components/Modal/Modal'
 const PaymentForm = () => {
   const { dataFormHome, setDataFormHome, isLogin, setIsLogin, toPay, setToPay } =
   useContext(AppContext)
+  const [showModal, setShowModal] = useState(false);
+  const [showAmount, setShowAmount] = useState('');
+  const navigate = useNavigate()
   const handlePayment=(data) => {
-   console.log(data)
+    setShowAmount(data.amount)
+   setShowModal(true);
+   
   }
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate('/')
+  };
   return (
     <div className='payment-form'>
+    {showModal?<Modal amount= {showAmount} onClose={handleCloseModal}/>:null}
       <FormPayment amount={toPay} onSubmit={handlePayment} />
     </div>
   )
