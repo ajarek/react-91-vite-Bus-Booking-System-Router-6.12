28995fc310503cfef7b94176a7aca16b8721e37c
Ajarek <ajarek@poczta.onet.pl>
2023-06-25 00:21:55 +0200

    added validation Form

--- src/helper/FormLogin/FormLogin.jsx
+++ src/helper/FormLogin/FormLogin.jsx
@@ -8,7 +8,7 @@ export const FormLogin = ({ onSubmit }) => {
   const schema = yup.object().shape({
     
     email: yup.string().email().required(),
-    password: yup.string().required(),
+    password: yup.string().min(4).max(12).required(),
    
   })
 
--- src/helper/FormPayment/FormPayment.jsx
+++ src/helper/FormPayment/FormPayment.jsx
@@ -7,9 +7,34 @@ import { useEffect } from 'react'
 export const FormPayment = ({ onSubmit, amount }) => {
   const schema = yup.object().shape({
     amount: yup.string(),
-    cardNumber: yup.string().required(),
-    expiryDate: yup.string().required(),
-    cvv:yup.string().required(),
+    cardNumber: yup
+      .string()
+      .matches(
+        /^(?:\d{4} ){3}\d{4}$/,
+        'Invalid credit card number format (e.g., 1234 5678 9012 3456)'
+      )
+      .required('Credit card number is required'),
+    expiryDate: yup
+      .string()
+      .matches(
+        /^(0[1-9]|1[0-2])\/20[2-9][0-9]$/,
+        'Invalid date format (e.g., 12/2023)'
+      )
+      .test(
+        'expirationDate',
+        'Year must not be less than the current year',
+        function (value) {
+          if (!value) return true // Skip validation if the field is empty
+          const currentYear = new Date().getFullYear()
+          const selectedYear = parseInt(value.split('/')[1])
+          return selectedYear >= currentYear
+        }
+      )
+      .required('Expiration date is required'),
+    cvv: yup
+      .string()
+      .matches(/^\d{3}$/, 'Invalid CVV format (must be 3 digits)')
+      .required('CVV is required'),
   })
 
   const {
@@ -25,16 +50,19 @@ export const FormPayment = ({ onSubmit, amount }) => {
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
       reset({
-        amount:'',
+        amount: '',
         cardNumber: '',
-        expiryDate:'',
-        cvv:''
+        expiryDate: '',
+        cvv: '',
       })
     }
   }, [formState, reset])
 
   return (
-    <form className='form-payment' onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className='form-payment'
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className='wrapper-input'>
         <label htmlFor='cardNumber'>Amount</label>
         <input
@@ -63,7 +91,7 @@ export const FormPayment = ({ onSubmit, amount }) => {
         />
         <p>{errors.expiryDate?.message}</p>
       </div>
-     
+
       <div className='wrapper-input'>
         <label htmlFor='cvv'>CVV</label>
         <input
@@ -73,7 +101,7 @@ export const FormPayment = ({ onSubmit, amount }) => {
         />
         <p>{errors.cvv?.message}</p>
       </div>
-     
+
       <div className='wrapper-input'>
         <input
           type='submit'
